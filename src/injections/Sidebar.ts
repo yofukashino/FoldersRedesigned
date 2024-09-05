4d925fa8e4e308714d2401f5fc3ad5e41d423da1
yofukashino <alokender018@gmail.com>
2024-09-05 11:22:52 +0530

    fix

--- src/injections/Sidebar.ts
+++ src/injections/Sidebar.ts
@@ -11,22 +11,29 @@ export default async (): Promise<void> => {
     "Sidebar",
     ([{ className }]: [{ className: string }], res: React.ReactElement & Types.Tree) => {
       if (!className.includes("foldersRedesigned-sidebar")) return res;
-      const scroller = Utils.findInReactTree(
-        res,
-        (c: React.ReactElement & Types.Tree) =>
-          typeof c?.props?.onScroll === "function" &&
-          c?.props?.children.some((i) => i?.props?.["aria-label"] === i18n.Messages.SERVERS),
-        100,
-      ) as React.ReactElement & Types.Tree;
-      const servers = Utils.findInReactTree(
-        scroller,
-        (c: React.ReactElement & Types.Tree) => c?.props?.["aria-label"] === i18n.Messages.SERVERS,
-        100,
-      ) as React.ReactElement & Types.Tree;
-      if (!scroller || !servers) {
-        return res;
-      }
-      scroller.props.children = servers.props.children;
+      const origial = res.props.children.props.children.bind(null);
+      res.props.children.props.children = (...args) => {
+        const ret = origial(...args);
+        const scroller = Utils.findInReactTree(
+          ret,
+          (c: React.ReactElement & Types.Tree) =>
+            typeof c?.props?.onScroll === "function" &&
+            c?.props?.children.some((i) => i?.props?.["aria-label"] === i18n.Messages.SERVERS),
+          100,
+        ) as React.ReactElement & Types.Tree;
+        const servers = Utils.findInReactTree(
+          scroller,
+          (c: React.ReactElement & Types.Tree) =>
+            c?.props?.["aria-label"] === i18n.Messages.SERVERS,
+          100,
+        ) as React.ReactElement & Types.Tree;
+        console.log(res);
+        if (!scroller || !servers) {
+          return res;
+        }
+        scroller.props.children = servers.props.children;
+        return ret;
+      };
       return res;
     },
   );
